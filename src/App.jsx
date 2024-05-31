fd6eb05d021688dfea8eb514149ad8c33f2e43ba
sunnykurmi <shivanibillore22@gmail.com>
2024-05-31 21:30:12 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -361,8 +361,6 @@ export default function App() {
 //           zoom={0.5}
 //           polar={[-0.1, Math.PI / 4]}
 //         >
-
-
 //           <Stage environment={null}>
 //             <Model scale={0.01}></Model>
 //           </Stage>
